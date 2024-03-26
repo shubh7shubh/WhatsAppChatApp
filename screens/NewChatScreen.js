0da4ce9708ced7b1f09756a58a87583cfc3fac7c
shubham <khobragade.shubham@gmail.com>
2024-03-26 17:57:47 +0530

    fixed the autocomplete off issue

--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -22,6 +22,8 @@ const NewChatScreen = props => {
 
     const userData = useSelector(state => state.auth.userData);
 
+    const isGroupChat = props.route.params && props.route.params.isGroupChat;
+
     useEffect(() => {
         props.navigation.setOptions({
             headerLeft: () => {
@@ -31,7 +33,7 @@ const NewChatScreen = props => {
                         onPress={() => props.navigation.goBack()} />
                 </HeaderButtons>
             },
-            headerTitle: "New chat"
+            headerTitle: isGroupChat ? "Add participants" : "New chat"
         })
     }, []);
 
@@ -71,6 +73,22 @@ const NewChatScreen = props => {
     }
 
     return <PageContainer>
+
+        {
+            isGroupChat &&
+            <View style={styles.chatNameContainer}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.textbox}
+                        placeholder="Enter a name for your chat"
+                        autoCorrect={false}
+                        autoComplete="off" // Corrected prop
+                    />
+                </View>
+            </View>
+        }
+
+
         <View style={styles.searchContainer}>
             <FontAwesome name="search" size={15} color={colors.lightGrey} />
 
@@ -156,6 +174,23 @@ const styles = StyleSheet.create({
     },
     noResultsText: {
         color: colors.textColor,
+        fontFamily: 'regular',
+        letterSpacing: 0.3
+    },
+    chatNameContainer: {
+        paddingVertical: 10
+    },
+    inputContainer: {
+        width: '100%',
+        paddingHorizontal: 10,
+        paddingVertical: 15,
+        backgroundColor: colors.nearlyWhite,
+        flexDirection: 'row',
+        borderRadius: 2
+    },
+    textbox: {
+        color: colors.textColor,
+        width: '100%',
         fontFamily: 'regular',
         letterSpacing: 0.3
     }
